76993099354b16baea71354d4ed6803c37b93b13
David England <david.england.de@googlemail.com>
2025-06-08 20:25:32 +0100

    Make fetch API call to web service (it fails)
    The call is made on page load, not on button click; currently, the button is not available.
    Further, the API call fails since fetch() doesn't like GETs with bodies. In all probability, I need
    to turn the GET into a POST in the web service.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,38 @@
-'use client'
-
-import { useState } from "react";
+import { Suspense } from "react";
 import './reset.css'
 import './globals.css'
 import Button from "@/components/button/button";
 import SolutionPane from "@/components/solution-pane/solution-pane";
 
 const testSudoku: Number[][] = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3],
-    [4, 4, 4, 4, 4, 4, 4, 4, 4],
-    [5, 5, 5, 5, 5, 5, 5, 5, 5],
-    [6, 6, 6, 6, 6, 6, 6, 6, 6],
-    [7, 7, 7, 7, 7, 7, 7, 7, 7],
-    [8, 8, 8, 8, 8, 8, 8, 8, 8],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
+    [0, 0, 0, 0, 0, 0, 9, 0, 2],
+    [0, 0, 0, 4, 0, 3, 0, 0, 0],
+    [4, 7, 9, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 7, 5, 0, 0, 0],
+    [0, 0, 5, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 3, 0, 6, 0, 0],
+    [0, 6, 0, 9, 0, 0, 0, 0, 0],
+    [0, 3, 0, 6, 0, 0, 0, 1, 0],
+    [0, 8, 4, 0, 0, 0, 0, 5, 0],
 ]
 
+async function query(input: Number[][]): Promise<Number[][][]> {
+  var response = await fetch("http://localhost:8081/sudoku", {
+    method: "GET",
+    body: JSON.stringify(testSudoku),
+  });
+
+  return await response.json();
+}
+
 export default function Home() {
-  const [solution, setSolution] = useState([] as Number[][][]);
+  var payload = query(testSudoku);
 
   return (
     <>
-      <Button callback={() => setSolution([...solution, testSudoku, testSudoku])} text={"Click me"} />
-      <SolutionPane solutionSteps={solution} />
+      <Suspense fallback={<p>Solution loading.... 🔃🔃🔃🔃</p>}>
+        <SolutionPane payload={payload} />
+      </Suspense>
     </>
   );
 }
--- src/components/solution-pane/solution-pane.tsx
+++ src/components/solution-pane/solution-pane.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import { use } from 'react';
 import styling from './solution-pane.module.css';
 import Sudoku from "../sudoku/sudoku";
 
-export default function SolutionPane({ solutionSteps }: { solutionSteps: Number[][][] }) {
+export default function SolutionPane({ payload }: { payload: Promise<Number[][][]> }) {
+    var solutionSteps = use(payload);
+
     return (
         <div className={styling.pane}>
             {solutionSteps.map((step, i) =>
